768c7ede86b5c6e1e6a945e2abc9d0eab36f097a
xieguigang <xie.guigang@gcmodeller.org>
2020-10-18 15:21:46 +0800

    Create export-images.R

---
+++ Rscript/export-images.R
@@ -0,0 +1,6 @@
+imports "voyager1" from "voyager";
+imports "wav" from "signalKit";
+
+const goldenRecord as string = "J:\GoogleDrive\Voyager\384kHzStereo.wav";
+
+# A demo R# script for export all images in the goden record 
