a8f53435bf2d2001c82d813c2604f79c16c980ea
xieguigang <xie.guigang@gcmodeller.org>
2020-10-18 11:50:59 +0800

    Update first-circle.R

--- Rscript/first-circle.R
+++ Rscript/first-circle.R
@@ -1,6 +1,11 @@
 imports "voyager1" from "voyager";
+imports "wav" from "signalKit";
 
 # R script for image parameter calibration
 
 const goldenRecord as string = "J:\GoogleDrive\Voyager\384kHzStereo.wav";
 
+let wav = read.wav(file = goldenRecord);
+
+# view of the raw file data summary;
+print(wav);
