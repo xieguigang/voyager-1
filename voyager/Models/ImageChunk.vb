db02443e3baadb94df589d7de7fd2b41b0268f16
xieguigang <xie.guigang@gcmodeller.org>
2020-10-18 11:40:59 +0800

    data chunk model

--- voyager/Models/ImageChunk.vb
+++ voyager/Models/ImageChunk.vb
@@ -1,3 +1,9 @@
-﻿Public Class ImageChunk
+﻿Imports Microsoft.VisualBasic.Data.Wave
+
+Public Class ImageChunk
+
+    Public Property channel As ChannelPositions
+    Public Property start As Long
+    Public Property length As Long
 
 End Class
--- voyager/Rscript.vb
+++ voyager/Rscript.vb
@@ -1,6 +1,8 @@
 ﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("voyager1")>
+<RTypeExport("chunk", GetType(ImageChunk))>
 Module Rscript
 
 
