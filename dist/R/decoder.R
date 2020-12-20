0a1a3d05ec5948acb34e45ced150f809842948d6
xieguigang <xie.guigang@gcmodeller.org>
2020-12-20 22:31:38 +0800

    renames symbol

--- dist/R/decoder.R
+++ dist/R/decoder.R
@@ -3,23 +3,23 @@ imports "wav" from "signalKit";
 
 const goldenRecord as string = "J:\GoogleDrive\Voyager\384kHzStereo.wav";
 
-let decode as function(start as integer, wavFile as string = goldenRecord, saveImg = "./img.png") {
+let decodeImage as function(start as integer, wavFile as string = goldenRecord, saveImg = "./img.png") {
 	using wav as read.wav(file = file(wavFile), lazy = TRUE) {
 		# view of the raw file data summary;
 		print(wav);
 
 		# parameters of the image
 		# and wav decoder arguments
-		let first_circle = new image.chunk(channel = "Left", start = start, length = 1928181);
+		let chunk = new image.chunk(channel = "Left", start = start, length = 1928181);
 		let decoder = new decode(windowSize = 3400, offset = 217);
 		
 		print("data size of this image chunk:");
-		print(wav :> chunk_size(chunk = first_circle));
+		print(wav :> chunk_size(chunk = chunk));
 
 		# run decoder and save the
 		# result image file
 		wav 
-		:> decode(chunk = first_circle, decode = decoder)
+		:> decode(chunk = chunk, decode = decoder)
 		:> as.bitmap(white = 1.125)
 		:> bitmap(file = saveImg)
 		;
--- dist/test/first-circle.R
+++ dist/test/first-circle.R
@@ -1,2 +1,2 @@
 # R# first-circle.R --attach voyager1_1.0.0.1254.zip
-decode(6000208, wavFile = goldenRecord, saveImg = `${!script$dir}/docs/circle.png`);
+decodeImage(6000208, wavFile = goldenRecord, saveImg = `${!script$dir}/circle.png`);
