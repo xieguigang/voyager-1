73a913ec4f5d8520ba9e4cde27c312e1a31ab68c
xieguigang <xie.guigang@gcmodeller.org>
2021-01-01 02:30:37 +0800

    run demo test success

--- dist/test/first-circle.R
+++ dist/test/first-circle.R
@@ -1,5 +1,7 @@
 # R# first-circle.R --attach voyager1_1.0.0.1254.zip
 
+require(voyager1);
+
 # raw wav data
 const goldenRecord as string = "J:/GoogleDrive/Voyager/384kHzStereo.wav";
 const demo_saveImg as string = `${!script$dir}/circle.png`;
