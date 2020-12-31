15bcf87089e923f066383d97cc8e1fcdca57819f
xieguigang <xie.guigang@gcmodeller.org>
2021-01-01 03:02:59 +0800

    add data chunk definitions

--- dist/R/chunks.R
+++ dist/R/chunks.R
@@ -1,4 +1,6 @@
 const data_chunks = list(
-    "Calibration_circle" = 6000208,
-    "Solar_location_map" = 8403580
+    "Calibration_circle"        = 6000208,
+    "Solar_location_map"        = 8465560,
+    "Mathematical_definitions"  = 10686671,
+    "Physical_unit_definitions" = 13001000
 );
