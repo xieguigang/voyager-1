cb9bcfe16966eed7b1dd3d32eac343bf7d46e064
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-08-24 18:22:56 +0800

    run R package msbuild

--- .pkg/build-unix.cmd
+++ .pkg/build-unix.cmd
@@ -5,7 +5,7 @@ SET R_HOME=%drive%\GCModeller\src\R-sharp\App\net6.0
 SET Rscript="%R_HOME%/Rscript.exe"
 SET REnv="%R_HOME%/R#.exe"
 
-%Rscript% --build /src ../ /save ../NRRD.zip --skip-src-build
-%REnv% --install.packages ../NRRD.zip
+%Rscript% --build /src ../ /save ../voyager-1.zip --skip-src-build
+%REnv% --install.packages ../voyager-1.zip
 
 pause
---
+++ @export/index.d.ts
@@ -0,0 +1,22 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=voyager1
+
+declare namespace voyager1 {
+   module _ {
+      /**
+      */
+      function onload(): object;
+   }
+   data_chunks: any;
+   /**
+     * @param offsetLeft default value Is ``0.15``.
+     * @param offsetRight default value Is ``0.1``.
+     * @param white default value Is ``0.8``.
+     * @param luminous default value Is ``false``.
+   */
+   function decodeImage(start: object, wavFile: string, offsetLeft?: any, offsetRight?: any, white?: any, luminous?: any): object;
+   /**
+   */
+   function export_from_chunks(wavFile: string, output_dir: string): object;
+}
