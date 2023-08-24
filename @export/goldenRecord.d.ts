7bd755e285418f9975b54b8a366460cbb6d5e45d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-08-24 18:31:04 +0800

    build assembly packages

---
+++ @export/goldenRecord.d.ts
@@ -0,0 +1,44 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "goldenRecord" from "voyager-1";
+//
+// ref=voyager_1.Rscript@voyager-1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace goldenRecord {
+   module as {
+      /**
+        * @param luminous default value Is ``false``.
+      */
+      function bitmap(pixels: object, luminous?: boolean): object;
+   }
+   /**
+    * measure the data chunk size of the wav data for current image chunk
+    * 
+    * 
+     * @param wav -
+     * @param chunk -
+   */
+   function chunk_size(wav: object, chunk: object): object;
+   /**
+    * Decode the wav data as the pixel scans
+    * 
+    * 
+     * @param wav -
+     * @param chunk -
+     * @param decode -
+     * @param size -
+     * 
+     * + default value Is ``364``.
+     * @param offsetLeft 
+     * + default value Is ``0.2``.
+     * @param offsetRight 
+     * + default value Is ``0.2``.
+     * @param env -
+     * 
+     * + default value Is ``null``.
+   */
+   function decode(wav: object, chunk: object, decode: object, size?: object, offsetLeft?: number, offsetRight?: number, env?: object): object;
+}
---
+++ man/voyager-1/goldenRecord/goldenRecord.md
@@ -0,0 +1,7 @@
+# goldenRecord
+
+
+
++ [decode](goldenRecord/decode.1) Decode the wav data as the pixel scans
++ [as.bitmap](goldenRecord/as.bitmap.1) 
++ [chunk_size](goldenRecord/chunk_size.1) measure the data chunk size of the wav data for current image chunk
