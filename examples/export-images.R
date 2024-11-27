ae4a27031f1f0ca032f063630ebfed7e3b0ce8c9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-27 22:24:23 +0800

    Update export-images.R

--- examples/export-images.R
+++ examples/export-images.R
@@ -1,7 +1,11 @@
-imports "voyager1" from "voyager";
+require(voyager1);
+
+imports "goldenRecord" from "voyager-1";
 imports "wav" from "signalKit";
 
-const goldenRecord as string = "J:\GoogleDrive\Voyager\384kHzStereo.wav";
+const goldenRecord as string = "../docs/384kHzStereo.wav";
+
+setwd(@dir);
 
 # A demo R# script for export all images 
 # in the goden record 
@@ -9,7 +13,7 @@ using wav as read.wav(file = file(goldenRecord), lazy = TRUE) {
     # parameters of the first circle image
     # and wav decoder arguments
     let image_chunk = new image.chunk(channel = "Left", start = 6000208, length = 1928181);
-    let decoder = new decode(windowSize = 3400, offset = 217);
+    let decoder     = new decode(windowSize = 3400, offset = 217);
 
 	# try to decode all images in the wav files
 	# its left channel
@@ -17,9 +21,9 @@ using wav as read.wav(file = file(goldenRecord), lazy = TRUE) {
 		# run decoder and save the
 		# result image file
 		wav 
-		:> decode(chunk = image_chunk, decode = decoder, size = 384)
-		:> as.bitmap(white = 0)
-		:> bitmap(file = `./test/${as.object(image_chunk)$start}.png`)
+		|> decode(chunk = image_chunk, decode = decoder, size = 384)
+		|> as.bitmap(white = 0)
+		|> bitmap(file = `Z:/test/${as.object(image_chunk)$start}.png`)
 		;		
 		
 		print(index);
